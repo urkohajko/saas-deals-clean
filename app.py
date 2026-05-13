3409eb1befa42129dec484965859c6bbd5189604
urkohajko <urkohajko@gmail.com>
2026-05-13 21:14:27 +0200

    Add root route

--- app.py
+++ app.py
@@ -2,18 +2,15 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import uvicorn
 
 app = FastAPI()
 
-# Blindaje absoluto de rutas
+# Static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Templates
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("deals.html", {"request": request})
-
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
